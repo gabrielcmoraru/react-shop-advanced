1035d0feb6fc6052d84f03d7e97e1006ae6c7299
Gabriel Moraru <gabriel@velocitypartners.com>
2020-06-14 20:00:55 +0100

    add sign in, show nav routes if logged in | frontend & backend

--- frontend/components/Signin.js
+++ frontend/components/Signin.js
@@ -5,13 +5,9 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User';
 
-const SIGNUP_MUTATION = gql`
-    mutation SIGNUP_MUTATION(
-        $email: String!
-        $name: String!
-        $password: String!
-    ) {
-        signup(email: $email, name: $name, password: $password) {
+const SIGNIN_MUTATION = gql`
+    mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+        signin(email: $email, password: $password) {
             id
             email
             name
@@ -19,9 +15,8 @@ const SIGNUP_MUTATION = gql`
     }
 `;
 
-class Signup extends Component {
+class Signin extends Component {
     state = {
-        name: '',
         password: '',
         email: '',
     };
@@ -33,25 +28,24 @@ class Signup extends Component {
     render() {
         return (
             <Mutation
-                mutation={SIGNUP_MUTATION}
+                mutation={SIGNIN_MUTATION}
                 variables={this.state}
                 refetchQueries={[{ query: CURRENT_USER_QUERY }]}
             >
-                {(signup, { error, loading }) => (
+                {(signin, { error, loading }) => (
                     <Form
                         method='post'
                         onSubmit={async (e) => {
                             e.preventDefault();
-                            const rest = await signup();
+                            const rest = await signin();
                             this.setState({
-                                name: '',
                                 email: '',
                                 password: '',
                             });
                         }}
                     >
                         <fieldset disabled={loading} aria-busy={loading}>
-                            <h2>Sign Up For An Account</h2>
+                            <h2>Sign Into Your Account</h2>
                             <Error error={error} />
                             <label htmlFor='email'>
                                 Email
@@ -61,17 +55,6 @@ class Signup extends Component {
                                     id='email'
                                     placeholder='email'
                                     value={this.state.email}
-                                    onChange={this.saveToState}
-                                />
-                            </label>
-                            <label htmlFor='name'>
-                                Name
-                                <input
-                                    type='text'
-                                    name='name'
-                                    id='name'
-                                    placeholder='name'
-                                    value={this.state.name}
                                     onChange={this.saveToState}
                                 />
                             </label>
@@ -86,7 +69,7 @@ class Signup extends Component {
                                     onChange={this.saveToState}
                                 />
                             </label>
-                            <button type='submit'>Sign Up!</button>
+                            <button type='submit'>Sign In</button>
                         </fieldset>
                     </Form>
                 )}
@@ -94,4 +77,4 @@ class Signup extends Component {
         );
     }
 }
-export default Signup;
+export default Signin;
