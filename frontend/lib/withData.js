997e527644928a3aceeec553c3074baa62e9eb10
Gabriel Moraru <gabriel@velocitypartners.com>
2020-06-15 22:51:10 +0100

    add cart component + query + mutation to trigger it | frontend & backend

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,19 +1,41 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
-  return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          credentials: 'include',
+    return new ApolloClient({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+        request: (operation) => {
+            operation.setContext({
+                fetchOptions: {
+                    credentials: 'include',
+                },
+                headers,
+            });
         },
-        headers,
-      });
-    },
-  });
+        // local state data
+        clientState: {
+            resolvers: {
+                Mutation: {
+                    toggleCart(_, variables, { cache }) {
+                        // read the cart open value
+                        const { cartOpen } = cache.readQuery({
+                            query: LOCAL_STATE_QUERY,
+                        });
+                        const data = {
+                            data: { cartOpen: !cartOpen },
+                        };
+                        cache.writeData(data);
+                        return data;
+                    },
+                },
+            },
+            defaults: {
+                cartOpen: true,
+            },
+        },
+    });
 }
 
 export default withApollo(createClient);
